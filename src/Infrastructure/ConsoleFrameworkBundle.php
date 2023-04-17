69fc77aa63540a7d863209186991956cf7cad2b5
vitaliy <zndev@ya.ru>
2023-04-17 20:54:49 +0600

    upd

---
+++ src/Infrastructure/ConsoleFrameworkBundle.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Untek\Framework\Console\Infrastructure;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Untek\Core\Kernel\Bundle\BaseBundle;
+
+class ConsoleFrameworkBundle extends BaseBundle
+{
+    public function getName(): string
+    {
+        return 'console-framework';
+    }
+
+    public function build(ContainerBuilder $containerBuilder)
+    {
+        $this->importServices($containerBuilder, __DIR__ . '/DependencyInjection/Symfony/services/console.php');
+    }
+}

5a5c01222d611bd2321d04687e81acd88fcc06e5
vitaliy <theyamshikov@yandex.ru>
2023-12-12 23:08:13 +0600

    upd

--- src/Domain/Shell/Shell.php
+++ src/Domain/Shell/Shell.php
@@ -8,6 +8,13 @@ use Untek\Framework\Console\Domain\Interfaces\ShellInterface;
 class Shell extends BaseShellNew implements ShellInterface
 {
 
+    private ?string $sudoPassword = null;
+
+    public function setSudoPassword(string $sudoPassword): void
+    {
+        $this->sudoPassword = $sudoPassword;
+    }
+
     public function runCmd(string $cmd): string
     {
         $command = '';
@@ -15,6 +22,9 @@ class Shell extends BaseShellNew implements ShellInterface
             $command .= "cd \"{$this->path}\" && ";
         }
         $command .= $cmd;
+        if($this->sudoPassword) {
+            $command = str_replace('sudo', 'echo "' . $this->sudoPassword . '" | sudo -S -k', $command);
+        }
         return $this->runCommand($command);
     }
 }

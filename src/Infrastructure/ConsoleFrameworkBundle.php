eab0152f489561f22a2ee6cf2c57ca39c794747c
vitaliy <theyamshikov@yandex.ru>
2024-01-08 11:15:36 +0600

    upd

--- src/Infrastructure/ConsoleFrameworkBundle.php
+++ src/Infrastructure/ConsoleFrameworkBundle.php
@@ -3,7 +3,10 @@
 namespace Untek\Framework\Console\Infrastructure;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Untek\Core\Code\Helpers\DeprecateHelper;
 use Untek\Core\Kernel\Bundle\BaseBundle;
+
+DeprecateHelper::hardThrow();
 
 class ConsoleFrameworkBundle extends BaseBundle
 {

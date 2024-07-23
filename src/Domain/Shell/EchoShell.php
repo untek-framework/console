b389c5980172bfb21f5422b7556dbea7568e8606
Vitaliy <theyamshikov@yandex.ru>
2024-07-23 14:59:38 +0500

    upd

--- src/Domain/Shell/EchoShell.php
+++ src/Domain/Shell/EchoShell.php
@@ -5,9 +5,10 @@ namespace Untek\Framework\Console\Domain\Shell;
 use Symfony\Component\Process\Exception\ProcessFailedException;
 use Symfony\Component\Process\Process;
 use Untek\Component\Measure\Time\Enums\TimeEnum;
+use Untek\Framework\Console\Domain\Helpers\CommandLineHelper;
 use Untek\Framework\Console\Domain\Interfaces\ShellInterface;
 
-class LocalShell implements ShellInterface
+class EchoShell implements ShellInterface
 {
 
     private ?string $sudoPassword = null;
@@ -20,26 +21,44 @@ class LocalShell implements ShellInterface
         $this->callback = $callback;
     }
 
-    public function setPath(string $path): void
+    /*public function setPath(string $path): void
     {
         $this->path = $path;
-    }
+    }*/
 
     public function runCmd(string $cmd, string $path = null): string
     {
-        $shell = new Shell($this->path);
+//        dd($cmd, $path);
+//        $shell = new Shell($this->path);
         $cmd = $this->prepareCmd($cmd, $path);
+//        dd($cmd);
+//        $escapedCmd = str_replace('"', '\"', $cmd);
         return $this->runCommandRaw($cmd);
+//        return parent::runCmd($cmd);
     }
 
     public function runCommandRaw(string $commandString, ?string $path = null): string
     {
+        echo PHP_EOL . $commandString . '(cwd: ' . $path . ')' . PHP_EOL;
+        return '';
+
+//        $commandString = $this->prepareCommandString($commandString);
+//        dump($commandString);
+
         $process = Process::fromShellCommandline($commandString, $path);
         $process->setTimeout(TimeEnum::SECOND_PER_YEAR);
+
+        /*$process->run();
+        if (!$process->isSuccessful()) {
+            throw new ProcessFailedException($process);
+        }*/
+
         $process->run($this->callback);
         if (!$process->isSuccessful()) {
             throw new ProcessFailedException($process);
         }
+//        CommandLineHelper::run($process);
+
         $commandOutput = $process->getOutput();
         return $commandOutput;
     }
--- src/Domain/Shell/RemoteShell.php
+++ src/Domain/Shell/RemoteShell.php
@@ -20,12 +20,10 @@ class RemoteShell extends LocalShell implements ShellInterface
 
     public function runCmd(string $cmd, string $path = null): string
     {
-//        $shell = new Shell($this->path);
         $cmd = $this->prepareCmd($cmd, $path);
         $ssh = $this->prepareSsh();
         $escapedCmd = str_replace('"', '\"', $cmd);
         return $this->runCommandRaw("$ssh \"$escapedCmd\"");
-//        return parent::runCmd($cmd);
     }
 
     public function setPath(string $path): void
@@ -40,7 +38,7 @@ class RemoteShell extends LocalShell implements ShellInterface
         if ($config->getPasswordFile()) {
             $command .= "export SSH_ASKPASS=\"{$config->getPasswordFile()}\" && setsid ";
         } elseif ($config->getPassword()) {
-            $command .= "echo \"echo shee8Eem\" > /tmp/1 && chmod 777 /tmp/1 && ";
+            $command .= "echo \"echo {$config->getPassword()}\" > /tmp/1 && chmod 777 /tmp/1 && ";
             $command .= "export SSH_ASKPASS=\"/tmp/1\" && setsid ";
         }
         $command .= "ssh {$config->getUser()}@{$config->getHost()} -p {$config->getPort()}";
